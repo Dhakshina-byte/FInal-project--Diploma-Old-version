11e928e00f4d6efc774f22feb39da7f43d951c20
dhakshina-byte <dhakshinaperera330@gmail.com>
2024-12-10 04:55:37 +0530

    new accountant

--- FInal project/Login.Designer.cs
+++ FInal project/Login.Designer.cs
@@ -98,6 +98,7 @@
             this.textBox2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.Location = new System.Drawing.Point(162, 141);
             this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(157, 32);
             this.textBox2.TabIndex = 3;
             // 
--- FInal project/Program.cs
+++ FInal project/Program.cs
@@ -16,7 +16,7 @@ namespace FInal_project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new vehicle_owner());
+            Application.Run(new Login());
         }
     }
 }
